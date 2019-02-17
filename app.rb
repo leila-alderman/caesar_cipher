2f6873397dfac021cacc5f8f8503480e45809436
leila-alderman <leila.alderman@gmail.com>
2019-02-17 15:52:18 -0500

    Add Caesar cipher method

--- app.rb
+++ app.rb
@@ -4,3 +4,20 @@ require "sinatra/reloader"
 get "/" do
   erb :index, layout: :main
 end
+
+def caesar(word, shift)
+  chars = word.split("")
+  shifted = []
+  for char in chars
+      if char.ord >= 65 && char.ord <= 90
+          shifted_num = (char.ord + shift - 65) % 26 + 65
+          shifted.push(shifted_num.chr)
+      elsif char.ord >= 97 && char.ord <= 122
+          shifted_num = (char.ord + shift - 97) % 26 + 97
+          shifted.push(shifted_num.chr)
+      else 
+          shifted.push(char)
+      end
+  end
+  shifted.join("")
+end
